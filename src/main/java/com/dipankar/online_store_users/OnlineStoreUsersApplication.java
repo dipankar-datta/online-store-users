e097631b52ae2bf4e844cb168b3c3c077869749f
Dipankar Datta <dattapro5@gmail.com>
2025-07-01 23:34:55 +0530

    Initial commit

---
+++ src/main/java/com/dipankar/online_store_users/OnlineStoreUsersApplication.java
@@ -0,0 +1,13 @@
+package com.dipankar.online_store_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipankar/online_store_users/OnlineStoreUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipankar.online_store_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
